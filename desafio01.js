1bb365fd9404c2412ac1c23a1f2675f99a44752f
denner augusto <denner.augusto90@gmail.com>
2023-01-20 14:02:47 -0300

    desafio01 resolvido

---
+++ desafio01.js
@@ -0,0 +1,22 @@
+/* 
+1) Faça uma função que receba uma string e retorne verdadeiro caso ela seja palíndromo,
+do contrário ela deve retornar falso. (palíndromos são palavras ou frases que podem ser
+lidas da esquerda para a direita ou da direita para a esquerda.)
+*/
+console.clear()
+
+function palindromo(string){
+    const res = string.split("").reverse().join("")
+
+   
+
+    if(res === string){
+        return console.log(true);
+    }
+
+    return console.log(false);
+}
+
+palindromo('ata')
+palindromo('osso')
+palindromo('radar')
